1f759c66d82c25db5d21eefb1346046dd71a4db0
HThTn <hongthetien0612@gmail.com>
2020-07-10 13:09:14 +0700

    chi tiet san pham vs san pham

---
+++ san-pham/DBController.php
@@ -0,0 +1,191 @@
+<?php
+
+class DBController
+{
+    // Database Connection Properties
+    protected $host = 'localhost';
+    protected $user = 'root';
+    protected $password = '';
+    protected $database = "shopee";
+
+    // connection property
+    public $con = null;
+
+    // call constructor
+    public function __construct()
+    {
+        $this->con = mysqli_connect($this->host, $this->user, $this->password, $this->database);
+        if ($this->con->connect_error){
+            echo "Fail " . $this->con->connect_error;
+        }
+    }
+
+    public function __destruct()
+    {
+        $this->closeConnection();
+    }
+
+    // for mysqli closing connection
+    protected function closeConnection(){
+        if ($this->con != null ){
+            $this->con->close();
+            $this->con = null;
+        }
+    }
+    public function connect()
+    {
+        $GLOBALS['conn'] = new mysqli($this->host, $this->user, $this->password, $this->database);
+        $GLOBALS['conn']->set_charset("utf8");
+        if ($this->con->connect_error){
+            echo "Fail " . $this->con->connect_error;
+        }
+        // else {echo "true ";}
+        return $GLOBALS['conn'];
+    }
+    public function close()
+    {
+        mysqli_close($GLOBALS['conn']);
+    }
+}
+
+// Use to fetch product data
+class Product
+{
+    public $db = null;
+
+    public function __construct(DBController $db)
+    {
+        if (!isset($db->con)) return null;
+        $this->db = $db;
+    }
+
+    // fetch product data using getData Method
+    public function getData($table = 'item_type_detail JOIN product ON item_type_detail.item_id=product.item_id'){
+        $result = $this->db->con->query("SELECT * FROM {$table}");
+
+        $resultArray = array();
+
+        // fetch product data one by one
+        while ($item = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+            $resultArray[] = $item;
+        }
+
+        return $resultArray;
+    }
+
+    // get product using item id
+    public function getProduct($item_id = null, $table= 'product'){
+        if (isset($item_id)){
+            $result = $this->db->con->query("SELECT * FROM {$table} WHERE item_id={$item_id}");
+
+            $resultArray = array();
+
+            // fetch product data one by one
+            while ($item = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                $resultArray[] = $item;
+            }
+
+            return $resultArray;
+        }
+    }
+
+}
+// php cart class
+class Cart
+{
+    public $db = null;
+
+    public function __construct(DBController $db)
+    {
+        if (!isset($db->con)) return null;
+        $this->db = $db;
+    }
+
+    // insert into cart table
+    public  function insertIntoCart($params = null, $table = "cart"){
+        if ($this->db->con != null){
+            if ($params != null){
+                // "Insert into cart(user_id) values (0)"
+                // get table columns
+                $columns = implode(',', array_keys($params));
+
+                $values = implode(',' , array_values($params));
+
+                // create sql query
+                $query_string = sprintf("INSERT INTO %s(%s) VALUES(%s)", $table, $columns, $values);
+
+                // execute query
+                $result = $this->db->con->query($query_string);
+                return $result;
+            }
+        }
+    }
+
+    // to get user_id and item_id and insert into cart table
+    public  function addToCart($userid, $itemid){
+        if (isset($userid) && isset($itemid)){
+            $params = array(
+                "user_id" => $userid,
+                "item_id" => $itemid
+            );
+
+            // insert data into cart
+            $result = $this->insertIntoCart($params);
+            if ($result){
+                // Reload Page
+                // header("Location: " . $_SERVER['PHP_SELF']);
+            }
+        }
+    }
+
+    // delete cart item using cart item id
+    public function deleteCart($item_id = null, $table = 'cart'){
+        if($item_id != null){
+            $result = $this->db->con->query("DELETE FROM {$table} WHERE item_id={$item_id}");
+            if($result){
+                header("Location:" . $_SERVER['PHP_SELF']);
+            }
+            return $result;
+        }
+    }
+
+    // calculate sub total
+    public function getSum($arr){
+        if(isset($arr)){
+            $sum = 0;
+            foreach ($arr as $item){
+                $sum += floatval($item[0]);
+            }
+            return sprintf('%.2f' , $sum);
+        }
+    }
+
+    // get item_it of shopping cart list
+    public function getCartId($cartArray = null, $key = "item_id"){
+        if ($cartArray != null){
+            $cart_id = array_map(function ($value) use($key){
+                return $value[$key];
+            }, $cartArray);
+            return $cart_id;
+        }
+    }
+
+    // Save for later
+    public function saveForLater($item_id = null, $saveTable = "wishlist", $fromTable = "cart"){
+        if ($item_id != null){
+            $query = "INSERT INTO {$saveTable} SELECT * FROM {$fromTable} WHERE item_id={$item_id};";
+            $query .= "DELETE FROM {$fromTable} WHERE item_id={$item_id};";
+
+            // execute multiple query
+            $result = $this->db->con->multi_query($query);
+
+            if($result){
+                header("Location :" . $_SERVER['PHP_SELF']);
+            }
+            return $result;
+        }
+    }
+
+
+}
+?>
