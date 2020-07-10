fe8f2b4a0b802fdcc52fb9f61e78a04f05971563
= <phattrinh9@gmail.com>
2020-07-10 22:41:19 +0700

    fix database and table

--- admin/js/demo/datatables-demo.js
+++ admin/js/demo/datatables-demo.js
@@ -3,6 +3,5 @@ $(document).ready(function() {
   $('#dataTable').DataTable();
 });
 // Call the dataTables jQuery plugin
-// $(document).ready(function() {
-//   $('#adding_table').DataTable();
-// });
+
+
