04786c7107b547d2e807ab20d15bdca6227d5520
tqa309 <goodguygdty@gmail.com>
2020-07-10 21:20:31 +0700

    minor update 1.3

--- chi-tiet-san-pham/index.php
+++ chi-tiet-san-pham/index.php
@@ -106,23 +106,12 @@ $item_id = $params['id'];
                 html = "";
                 html += `<h1 class="font-baloo">${response.Name} </h1>`;
                 html += ` <small style="font-size:12px; font-style:italic"> by ${response.Brand}</small><hr class="m-0">`;
-                html+=`<div class="row"style="margin-top:20px">`
-<<<<<<< HEAD
+                html+=`<div class="row"style="margin-top:20px">`;
                 html += `<div class="col-6 col-lg-5 font-rale" style="font-size:16px;"> Giá gốc: </div>`;
                 html += `<div class="col-6 col-lg-7 font-rale" style="font-size:16px;"> <strike> ${response.Price * 10}</strike> </div>`;
                 html += `<div class="col-6 col-lg-5 font-rale" style="font-size:18px;margin-top:15px">Giá khuyến mãi: </div>`;
                 html += `<div class="col-6 col-lg-7 font-rale"><span style="font-size:26px;"class="text-danger">${price}</span> <br><small class="text-dark font-size-12">&nbsp;&nbsp;Bao gồm VAT 10%</small></div>`;
                 html += `</div>`;    
-                // html += `<table class="my-3"> <tbody><tr class="font-rale"style="font-size:18px;"><td>Giá gốc:</td><td><strike>$ ${response.Price * 100} </strike></td></tr>`;
-                // html += `<tr class="font-rale"style="font-size:18px;"><td>Giá khuyến mãi:</td><td style="font-size:26px;"class="text-danger">$<span>${response.Price}</span><small class="text-dark font-size-12">&nbsp;&nbsp;Bao gồm VAT 10%</small></td></tr>`;
-                // html += `</tbody> </table>`;
-=======
-                html += `<div class=" col-5 font-rale" style="font-size:14px;"> Giá gốc: </div>`;
-                html += `<div class=" col-7 font-rale" style="font-size:14px;"> <strike> ${response.Price * 10} đ</strike> </div>`;
-                html += `<div class=" col-5 font-rale" style="font-size:16px;margin-top:15px">Giá khuyến mãi: </div>`;
-                html += `<div class=" col-7 font-rale"><span style="font-size:24px;"class="text-danger">${response.Price} đ</span> <br><small class="text-dark font-size-12">&nbsp;&nbsp;Bao gồm VAT 10%</small></div>`;
-                html += `</div>`;
->>>>>>> e0d0178efb75a02848fc145fa126e1d530b937ab
                 html += `<div id="policy"><div class="d-flex"><div class="return text-center"style="margin-right: 3.5rem !important;">`;
                 html += `<div class="font-size-20 my-2 color-second"><span class="fas fa-retweet border p-3 rounded-pill"></span></div>`;
                 html += `<a href="#" class="font-rale font-size-16">10 ngày <br> đổi trả</a></div><div class="return text-center "style="margin-right: 3.5rem !important;"> <div class="font-size-20 my-2 color-second">`;
